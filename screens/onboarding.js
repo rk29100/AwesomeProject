7f593424e1efab6e488c4ecbbabd5c38dec6dfed
rk29100 <rsk29100@gmail.com>
2021-11-23 17:11:30 +0530

    icon updated

--- screens/onboarding.js
+++ screens/onboarding.js
@@ -1,6 +1,6 @@
 import Onboarding from 'react-native-onboarding-swiper';
 import React, { Component } from 'react';
-import {View,Image}  from 'react-native';
+import {View,Image,StyleSheet,Text}  from 'react-native';
 import Tabs from '../navigation/tabs';
 
 const onboarding=({navigation})=> {
@@ -12,25 +12,38 @@ const onboarding=({navigation})=> {
                     onDone={()=>{navigation && navigation.navigate('tabs', { screen: 'Home' })}}
 
                     pages={[
+                        
                         {
+                            title: <Text style={{position:'absolute',color:'#292f33',fontStyle:'italic',fontWeight:'bold',fontSize:60,textAlign:'center',lineHeight:60,top:40}}>SWASTHYA UPCHARA</Text>,
+                            // <Text style={{position: 'absolute',
+                            //     top: 50, fontSize:37, alignContent:'center',fontStyle:'italic',fontWeight:'bold',color:'#292f33'}}>SWASTHYA UPCHARA</Text>,
+                            subtitle: <Text style={{position:'absolute',color:'#292f33',fontStyle:'italic',fontWeight:'bold',fontSize:30,textAlign:'center',bottom:170}}>Heart of Perfect Health..!</Text>,
+                            // <Text style={{position: 'absolute',
+                            // bottom: 130,
+                            // fontSize:30, alignContent:'center',fontStyle:'italic',fontWeight:'bold',color:'#292f33'}}>Heart of Perfect Health..! </Text>,
                             backgroundColor: '#fff',
-                            image: <Image 
-                                source={require('../assets/icons/remedy.png')} 
-                                // resizeMode='center' 
-                                style={{width:300,height:300}} />,
-                            title: 'Onboarding',
-                            subtitle: 'Done with React Native Onboarding Swiper',
+                            image: <>
+                                <Image source={require('../assets/icons/yoga.png')}
+                                style={styles.ImageStyle}/>
+                                
+                            <Image
+                            source={require('../assets/icons/ayurveda.jpg')}
+                            style={styles.backgroundImageStyle}/>
+                            
+                            </>
+                    ,
+                            
                         },
-                        {
-                            backgroundColor: '#fff',
-                            image: <Image 
-                                    source={require('../assets/icons/home.png')} 
-                                    // resizeMode='center'
-                                    style={{width:300,height:300}}
-                                    />,
-                            title: 'Onboarding',
-                            subtitle: 'Done with React Native Onboarding Swiper',
-                        }
+                        // {
+                        //     backgroundColor: '#fff',
+                        //     image: <Image 
+                        //             source={require('../assets/icons/home.png')} 
+                        //             // resizeMode='center'
+                        //             style={{width:300,height:300}}
+                        //             />,
+                        //     title: 'Onboarding',
+                        //     subtitle: 'Done with React Native Onboarding Swiper',
+                        // }
                         ]}
 />
                 
@@ -40,3 +53,21 @@ const onboarding=({navigation})=> {
 }
 
 export default onboarding;
+
+const styles=StyleSheet.create({
+    backgroundImageStyle: {
+        // width:"100%",
+        // height:"50%",
+         position: 'absolute',
+        opacity: 0.18,
+        zIndex: 0,
+        width: 500,
+        height: 1000,
+        top:-340,
+        // transform:[{rotate:"90deg"}]
+      },
+      ImageStyle:{
+        position:'relative',
+                
+      }
+});
